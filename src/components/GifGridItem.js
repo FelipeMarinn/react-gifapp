37bc8f03e2f31074237da42d3067f7e177c33b7d
felipe <felipeguisao18@gmail.com>
2021-05-29 15:32:55 -0500

    cards animacion actualizada

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ({ id, tittle, url }) => {
     console.log({ id, tittle, url })
 
     return (
-        <div className='card animate__animated animate__fadeIn animate__delay-2s'>
+        <div className='card animate__animated animate__fadeIn'>
             <img src={ url } alt={ tittle } />
             <p> { tittle} </p>
         </div>
